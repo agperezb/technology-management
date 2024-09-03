dc69c7d6ed29a554fa43b98b09a0d56127f756aa
Angel Pérez <angel.perez@pragma.com.co>
2024-09-03 11:38:29 -0500

    first commit

---
+++ src/main/java/org/agperezb/technologymanagement/TechnologyManagementApplication.java
@@ -0,0 +1,13 @@
+package org.agperezb.technologymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/agperezb/technologymanagement/TechnologyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agperezb.technologymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
